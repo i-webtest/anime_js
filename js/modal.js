3c77094e128daa0a027640fb35eb1a5382379d43
i-webtest <stasbass80@gmail.com>
2023-07-30 12:39:30 +0300

    Search implementation

--- js/modal.js
+++ js/modal.js
@@ -4,6 +4,38 @@ const modal = () => {
   const modalClose = modal.querySelector('.search-close-switch');
   const form = modal.querySelector('.search-model-form');
   const searchInput = form.querySelector('#search-input');
+  const wrapper = document.querySelector('.search-model-result');
+
+  wrapper.style.width = '100%';
+  wrapper.style.maxWidth = '500px';
+
+  const renderFunc = (items) => {
+    wrapper.innerHTML = '';
+
+    items.forEach((item) => {
+      wrapper.insertAdjacentHTML(
+        'afterbegin',
+        `
+          <a class="p-2" href="/anime-details.html" target="_blank">${item.title}</a>
+        `,
+      );
+    });
+  };
+
+  const searchFunc = (searchStr) => {
+    fetch('https://anime-e0a14-default-rtdb.firebaseio.com/anime.json')
+      .then((res) => res.json())
+      .then((data) => {
+        const filterData = data.filter((dataItem) => {
+          return (
+            dataItem.title.toLowerCase().includes(searchStr.toLowerCase()) ||
+            dataItem.description.toLowerCase().includes(searchStr.toLowerCase())
+          );
+        });
+
+        renderFunc(filterData.slice(0, 5));
+      });
+  };
 
   modalBtn.addEventListener('click', () => {
     modal.style.display = 'block';
@@ -11,11 +43,13 @@ const modal = () => {
 
   modalClose.addEventListener('click', () => {
     modal.style.display = 'none';
+    searchInput.value = '';
+    wrapper.innerHTML = '';
   });
 
   searchInput.addEventListener('input', (e) => {
     const target = e.target.value;
-    console.log(target);
+    searchFunc(target);
   });
 };
 
